13bfa3ba295a1957b04b4eb482328f7824c5c6ca
jerrande <jerryleeandersonjr@gmail.com>
2023-08-01 11:02:00 -0400

    add base classes to models

--- flask_app/models/DNAC_template.py
+++ flask_app/models/DNAC_template.py
@@ -1,3 +1,14 @@
 from flask_app.config.mysqlconnection import connectToMySQL
 from flask_app.models import user
 from flask import flash
+
+db = "dnac_template_schema"
+class DNAC_Template:
+    def __init__(self, db_data):
+        self.id = db_data['id']
+        self.template_name = db_data['template_name']
+        self.template_body = db_data['template_body']
+        self.created_at = db_data['created_at']
+        self.updated_at = db_data['updated_at']
+        self.user_id = db_data['user_id']
+        self.creator = None
--- flask_app/models/user.py
+++ flask_app/models/user.py
@@ -2,3 +2,15 @@ from flask_app.config.mysqlconnection import connectToMySQL
 from flask import flash
 from flask_app import bcrypt
 import re
+
+db = "dnac_template_schema"
+EMAIL_REGEX = re.compile(r'^[a-zA-Z0-9.+_-]+@[a-zA-Z0-9._-]+\.[a-zA-Z]+$')
+class User:
+    def __init__(self, db_data):
+        self.id = db_data['id']
+        self.first_name = db_data['first_name']
+        self.last_name = db_data['last_name']
+        self.email = db_data['email']
+        self.password = db_data['password']
+        self.created_at = db_data['created_at']
+        self.updated_at = db_data['updated_at']
