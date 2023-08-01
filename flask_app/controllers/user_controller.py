0bd82bbad3bbf337c39ca6fc78061097b3b9de8e
jerrande <jerryleeandersonjr@gmail.com>
2023-08-01 11:42:00 -0400

    update user_controller with basic route logic

--- flask_app/controllers/user_controller.py
+++ flask_app/controllers/user_controller.py
@@ -1,3 +1,14 @@
 from flask_app import app
 from flask import render_template, redirect, request, session
 from flask_app.models.user import User
+
+@app.route('/')
+def index():
+    return redirect('/user/login')
+
+@app.route('/user/login')
+def login():
+    if 'user_id' in session:
+        return redirect('/dashboard')
+
+    return render_template('index.html')
