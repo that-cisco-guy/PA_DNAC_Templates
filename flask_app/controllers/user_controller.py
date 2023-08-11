3ae9747c1d74d5877e567bffc7a2a5bc6af1a103
jerrande <jerryleeandersonjr@gmail.com>
2023-08-11 09:17:24 -0400

    fix author on dashboard

--- flask_app/controllers/user_controller.py
+++ flask_app/controllers/user_controller.py
@@ -1,5 +1,5 @@
 from flask_app import app
-from flask import render_template, redirect, request, session
+from flask import render_template, redirect, request, session, flash
 from flask_app.models.user import User
 
 @app.route('/')
